9204125f563183309e0b8b5ad7fdf642ca4f6c00
mario <marioskrlec222@proton.me>
2024-10-28 13:06:30 +0100

    Added version tests

---
+++ tests/versions.spec.ts
@@ -0,0 +1,29 @@
+import {describe, expect, it} from 'vitest';
+import {workingTestProjects} from './projects';
+// @ts-ignore
+import {getVersions, initialize} from '@root';
+// @ts-ignore
+import {MapItem} from '@appTypes/types/Maps';
+describe('Version test', () => {
+    it("should get all versions of a project", async () => {
+        for (const projectId of workingTestProjects) {
+            initialize({
+                projectId: projectId,
+                baseUrl: "http://localhost:3002",
+            });
+
+            const {result, error} = await getVersions();
+
+            expect(error).equal(undefined);
+
+            for (const version of result) {
+                expect(version.id).not.toBe(undefined);
+                expect(version.projectId).equal(projectId);
+                expect(version.name).equal('v1');
+                expect(version.createdAt).not.toBe(undefined);
+                expect(version.createdAt).not.toBe(undefined);
+            }
+
+        }
+    });
+})
